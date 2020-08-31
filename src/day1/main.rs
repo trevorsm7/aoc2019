f99c9bcecad0616736aee24843a415de5f4755a5
Trevor Smith <trevorsm7@gmail.com>
2020-08-31 16:20:57 -0700

    Add day1, part 2

--- src/day1/main.rs
+++ src/day1/main.rs
@@ -21,13 +21,13 @@ fn main() -> Result<(), SuperError> {
         BufReader::new(file)
     };
 
-    let result: u32 = reader.lines()
+    let result: (u32, u32) = reader.lines()
         .filter_map(|s| s.ok())
         .filter_map(|s| s.parse().ok())
-        .map(compute_fuel)
-        .sum();
+        .map(|n| (compute_fuel(n), compute_fuel_fuel(n)))
+        .fold((0, 0), |acc, x| (acc.0 + x.0, acc.1 + x.1));
 
-    println!("{}", result);
+    println!("{:?}", result);
 
     Ok(())
 }
@@ -50,13 +50,35 @@ err
 */
 
 fn compute_fuel(input: u32) -> u32 {
-    input / 3 - 2
+    if input > 2 * 3 {
+        input / 3 - 2
+    } else {
+        0
+    }
 }
 
 #[test]
 fn test_compute_fuel() {
+    assert_eq!(compute_fuel(0), 0);
     assert_eq!(compute_fuel(12), 2);
     assert_eq!(compute_fuel(14), 2);
     assert_eq!(compute_fuel(1969), 654);
     assert_eq!(compute_fuel(100756), 33583);
+}
+
+fn compute_fuel_fuel(input: u32) -> u32 {
+    let mut total = 0;
+    let mut fuel = compute_fuel(input);
+    while fuel > 0 {
+        total += fuel;
+        fuel = compute_fuel(fuel);
+    }
+    total
+}
+
+#[test]
+fn test_compute_fuel_fuel() {
+    assert_eq!(compute_fuel_fuel(14), 2);
+    assert_eq!(compute_fuel_fuel(1969), 966);
+    assert_eq!(compute_fuel_fuel(100756), 50346);
 }
