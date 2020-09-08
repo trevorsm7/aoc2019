c72d77606b463b42cc253b112ca22fc2f9ee62fa
Trevor Smith <trevorsm7@gmail.com>
2020-09-07 17:47:38 -0700

    Add day 4, part 1

---
+++ src/day4/main.rs
@@ -0,0 +1,139 @@
+use std::env;
+
+fn main() {
+    let input = env::args().nth(1).expect("Expected argument ######-######")
+        .trim().split('-')
+        .map(|s| {
+            s.chars().map(|c| {
+                c.to_digit(10).expect(&format!("Invalid digit {}", c))
+            }).collect::<Vec<u32>>()
+        }).collect::<Vec<Vec<u32>>>();
+
+    let end = &input[1];
+    let mut current = first_monotonic_sequence(&input[0]);
+    let mut counter = 0;
+    while is_sequence_le(&current, end) {
+        if is_sequence_repeating(&current) {
+            counter += 1;
+        }
+        increment_monotonic_sequence(&mut current);
+    }
+
+    println!("{}", counter);
+}
+
+fn first_monotonic_sequence(input: &[u32]) -> Vec<u32> {
+    let mut max = 0;
+    let mut increasing = true;
+    input.iter().cloned()
+        .map(|i| {
+            if i < max {
+                increasing = false;
+            }
+            if increasing && i > max {
+                max = i;
+            }
+            max
+        }).collect()
+}
+
+#[test]
+fn test_first_monotonic_sequence() {
+    assert_eq!(first_monotonic_sequence(&[1, 2, 3]), &[1, 2, 3]);
+    assert_eq!(first_monotonic_sequence(&[1, 0, 3]), &[1, 1, 1]);
+    assert_eq!(first_monotonic_sequence(&[1, 2, 0]), &[1, 2, 2]);
+    assert_eq!(first_monotonic_sequence(&[2, 6, 4, 7, 9, 3]), &[2, 6, 6, 6, 6, 6]);
+}
+
+fn is_sequence_le(a: &[u32], b: &[u32]) -> bool {
+    for (da, db) in a.iter().zip(b.iter()) {
+        if da < db {
+            return true;
+        } else if da > db {
+            return false;
+        }
+    }
+    true
+}
+
+#[test]
+fn test_is_sequence_le() {
+    assert_eq!(is_sequence_le(&[1, 2, 3], &[1, 2, 3]), true);
+    assert_eq!(is_sequence_le(&[1, 2, 3], &[1, 2, 4]), true);
+    assert_eq!(is_sequence_le(&[1, 2, 3], &[2, 0, 0]), true);
+    assert_eq!(is_sequence_le(&[1, 2, 3], &[1, 2, 2]), false);
+}
+
+fn increment_monotonic_sequence(sequence: &mut [u32]) {
+    let mut replacement = 0;
+    for &digit in sequence.iter().rev() {
+        if digit < 9 {
+            replacement = digit + 1;
+            break;
+        }
+    }
+    for digit in sequence.iter_mut().rev() {
+        if *digit < 9 {
+            *digit += 1;
+            return;
+        } else {
+            *digit = replacement;
+        }
+    }
+}
+
+#[cfg(test)]
+fn test_increment_monotonic_sequence_helper(input: &[u32], output: &[u32]) {
+    let mut copy = Vec::from(input);
+    increment_monotonic_sequence(&mut copy);
+    assert_eq!(copy, output);
+}
+
+#[test]
+fn test_increment_monotonic_sequence() {
+    test_increment_monotonic_sequence_helper(&[1, 7, 9], &[1, 8, 8]);
+    test_increment_monotonic_sequence_helper(&[1, 8, 8], &[1, 8, 9]);
+    test_increment_monotonic_sequence_helper(&[1, 8, 9], &[1, 9, 9]);
+    test_increment_monotonic_sequence_helper(&[1, 9, 9], &[2, 2, 2]);
+    test_increment_monotonic_sequence_helper(&[8, 9, 9], &[9, 9, 9]);
+    test_increment_monotonic_sequence_helper(&[9, 9, 9], &[0, 0, 0]);
+}
+
+fn is_sequence_repeating(sequence: &[u32]) -> bool {
+    let mut prev = sequence[0];
+    let mut repeated = false;
+    for &digit in sequence.iter().skip(1) {
+        if digit == prev {
+            repeated = true;
+        }
+        prev = digit;
+    }
+    repeated
+}
+
+#[test]
+fn test_is_sequence_repeating() {
+    assert_eq!(is_sequence_repeating(&[1, 1, 1, 1, 1, 1]), true);
+    assert_eq!(is_sequence_repeating(&[2, 2, 3, 4, 5, 0]), true);
+    assert_eq!(is_sequence_repeating(&[1, 2, 3, 7, 8, 9]), false);
+}
+
+#[cfg(test)]
+fn is_sequence_monotonic(sequence: &[u32]) -> bool {
+    let mut max = sequence[0];
+    for &digit in sequence.iter().skip(1) {
+        if digit < max {
+            return false;
+        } else if digit > max {
+            max = digit;
+        }
+    }
+    true
+}
+
+#[test]
+fn test_is_sequence_monotonic() {
+    assert_eq!(is_sequence_monotonic(&[1, 1, 1, 1, 1, 1]), true);
+    assert_eq!(is_sequence_monotonic(&[2, 2, 3, 4, 5, 0]), false);
+    assert_eq!(is_sequence_monotonic(&[1, 2, 3, 7, 8, 9]), true);
+}
