e778691fea23e42805faf3c259e521de568aab4a
Trevor Smith <trevorsm7@gmail.com>
2020-10-06 17:50:22 -0700

    Add day 8, part 1

---
+++ src/bin/day8.rs
@@ -0,0 +1,49 @@
+#[macro_use]
+extern crate quick_error;
+
+use std::borrow::Cow;
+use std::env;
+use std::io;
+use std::num::ParseIntError;
+
+quick_error! {
+    #[derive(Debug)]
+    pub enum SuperError {
+        IoError(err: io::Error) { from() }
+        ParseIntError(err: ParseIntError) { from() }
+    }
+}
+
+type SuperResult<T> = Result<T, SuperError>;
+
+fn main() -> SuperResult<()> {
+    let input = {
+        let name: Cow<'static, str> = env::args().nth(1)
+            .map(|s| s.into()).unwrap_or_else(|| "input".into());
+        std::fs::read_to_string(name.as_ref())?
+    };
+
+    let data = input.trim().chars()
+        .map(|c| c.to_digit(10).ok_or_else(||
+            io::Error::new(io::ErrorKind::InvalidData, format!("Invalid digit {}", c))))
+        .collect::<io::Result<Vec<u32>>>()?;
+
+    let rows = 6;
+    let cols = 25;
+    let pixels = rows * cols;
+
+    let mut result = 0;
+    let mut fewest_zeros = u32::MAX;
+    for layer in data.chunks(pixels) {
+        let mut count = [0; 3];
+        layer.iter().for_each(|&pixel| count[pixel as usize] += 1);
+        if count[0] < fewest_zeros {
+            fewest_zeros = count[0];
+            result = count[1] * count[2];
+        }
+    }
+
+    println!("Part 1: {}", result);
+
+    Ok(())
+}
